741fd0e8b74aed56be8a24f05061e9d2fb2c79bb
Bashirubasit <basitbashiru8@gmail.com>
2025-09-07 14:46:15 +0100

    Fix About page quote issue

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,18 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div id='#about' className='p-10 text-center'>
+    <div id='about' className='p-10 text-center'>
         <h2 className="text-5xl font-normal text-center mb-8">
             <span className="text-orange-500">About</span>{" "}
             <span className="text-black">Us</span>
         </h2>
 
         <p className="text-lg text-gray-700 max-w-3xl mx-auto leading-relaxed">
-            Welcome to Aremu Shop, Aremu is a streetwear brand inspired 
-            by the ethos "The World is Yours." Based in Lagos Nigeria, 
-            Aremu redefines streetwear with a refined, high-quality approach, 
-            crafting timeless pieces that exude confidence and inspire individuality.
+          Welcome to Aremu Shop, Aremu is a streetwear brand inspired 
+          by the ethos &quot;The World is Yours.&quot; Based in Lagos Nigeria, 
+          Aremu redefines streetwear with a refined, high-quality approach, 
+          crafting timeless pieces that exude confidence and inspire individuality.
         </p>
+
     </div>
   )
 }
